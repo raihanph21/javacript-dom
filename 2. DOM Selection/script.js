7838db85d582c21cc343efbb30934efa24ffb028
raihanph21 <m.rhn.ph@gmail.com>
2022-11-20 17:29:33 +0700

    DOM selection id, TagName, ClassName

---
+++ 2. DOM Selection/script.js
@@ -0,0 +1,20 @@
+// DOM Selection
+// document.getElementById() -> element
+const judul = document.getElementById("judul");
+judul.style.color = "#1e81b0";
+judul.style.backgroundColor = "#ccc";
+judul.innerHTML = "Muhammad Raihan";
+
+// document.getElementsByTagName() -> HTMLCollections
+const p = document.getElementsByTagName("p");
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = "lightblue";
+}
+
+const h1 = document.getElementsByTagName("h1")[0]; // -> karna getElementsByTagName akan mengembalikkan HTMLCollections
+h1.style.fontSize = "50px";
+
+// document.getElementsByClassName() -> HTMLCollections
+const p1 = document.getElementsByClassName("p1");
+p1[0].innerHTML = "Ini diubah dari javascript"; // cara 2 buat masukkin index [0]
